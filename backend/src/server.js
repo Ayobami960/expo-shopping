ef8bb779912db4d225c454b567f50c8ce7c3b3c1
Ayobami960 <adetoye2021ayobami2@gmail.com>
2025-12-06 18:04:28 +0100

    deplayment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,31 @@
 import express from "express";
-
+import path from "path";
+import { ENV } from "./config/env.js";
 
 
 const app = express();
+
+const __dirname = path.resolve();
 
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Success" });
 });
+
+// make our app ready for deplayment
+if (ENV.NODE_ENV === "production") {
+    // if you are in the production envronment take all admin footer and the dist footer should be a dist asset
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+    // if we reset any route other then the api route we have right to see a react application
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    })
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
+    // });
+
+}
 
 
 const startServer = async () => {
