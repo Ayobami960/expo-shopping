4e29dab01ec6332e8ef5742ea78f68c85ffe10f3
Ayobami960 <adetoye2021ayobami2@gmail.com>
2025-12-10 12:59:47 +0100

    fix for master

--- backend/src/models/order.model.js
+++ backend/src/models/order.model.js
@@ -1,93 +1,96 @@
 import mongoose from "mongoose";
 
 const orderItemSchema = new mongoose.Schema({
-    product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true,
-        min:0
-    },
-    quantity: {
-        type: Number,
-        required: true,
-        min:1
-    },
-    image: {
-        type: String,
-        required: true,
-    },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
 });
 
 const shippingAddressSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: true
-    },
-    streetAddress: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    state: {
-        type: String,
-        required: true
-    },
-    zipCode: {
-        type: String,
-        required: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true
-    }
-})
+  fullName: {
+    type: String,
+    required: true,
+  },
+  streetAddress: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+});
 
-const orderSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     clerkId: {
-        type: String,
-        required: true, 
+      type: String,
+      required: true,
     },
     orderItems: [orderItemSchema],
     shippingAddress: {
-        type: shippingAddressSchema,
-        required: true
+      type: shippingAddressSchema,
+      required: true,
     },
     paymentResult: {
-        id: String,
-        status: String
+      id: String,
+      status: String,
     },
     totalPrice: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     status: {
-        type: String,
-        enum: ["pending", "shipped", "delivered"],
-        default: "pending"
+      type: String,
+      enum: ["pending", "shipped", "delivered"],
+      default: "pending",
     },
     deliveredAt: {
-        type: Date
+      type: Date,
     },
     shippedAt: {
-        type: Date
-    }
-}, { timestamps: true });
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
 
-export const Order = mongoose.model("Order", orderSchema)
+export const Order = mongoose.model("Order", orderSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,7 @@ import { connectDB } from "./config/db.js";
 
 import adminRoutes from "./routes/admin.route.js"
 import userRoutes from "./routes/user.route.js"
+import orderRoutes from "./routes/order.route.js"
 
 
 
@@ -23,8 +24,9 @@ app.use(clerkMiddleware()); // this middle where adds auth object under the req 
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.use("api/admin", adminRoutes)
-app.use("api/users", userRoutes)
+app.use("/api/admin", adminRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/orders", orderRoutes);
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Success" });
