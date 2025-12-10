8f32fab210b4fe803e6c307ac9066e328d765d25
Ayobami960 <adetoye2021ayobami2@gmail.com>
2025-12-10 14:33:58 +0100

    cart routes & controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { clerkMiddleware } from "@clerk/express"
 import { serve } from "inngest/express"
+import cors from "cors"
 
 import { functions, inngest } from "./config/inngest.js";
 
@@ -14,6 +15,7 @@ import userRoutes from "./routes/user.route.js"
 import orderRoutes from "./routes/order.route.js"
 import reviewsRoutes from "./routes/review.route.js"
 import productRoutes from "./routes/product.route.js"
+import cartRoutes from "./routes/cart.route.js"
 
 
 
@@ -23,14 +25,17 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(clerkMiddleware()); // this middle where adds auth object under the req => req.auth
+// credentials: true allows the browser to send the cookies to the server with the request
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.use("/api/admin", adminRoutes)
-app.use("/api/users", userRoutes)
+app.use("/api/admin", adminRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/reviews", reviewsRoutes);
 app.use("/api/product", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Success" });
